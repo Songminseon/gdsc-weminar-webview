1e92d9a3e78a2a26fbbce8e3144f7b71dfcd250b
Songminseon <smsun0329@naver.com>
2021-10-14 22:54:20 +0900

    안드로이드 웹뷰 어플리케이션

---
+++ app/src/main/java/com/example/gsdc_webview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gsdc_webview;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // 앱에서 표시할 url 입력
+        webView.loadUrl("https://do.funation.io");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
